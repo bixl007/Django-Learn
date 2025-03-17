135e0858dd77cc9d1b3d1da187e28ffb788a14a6
Bishal Baira <bishalbaira432@gmail.com>
2025-03-17 22:07:40 +0530

    Django Relationships

--- djangoBE/ZodApp/models.py
+++ djangoBE/ZodApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -11,9 +12,39 @@ class ChaiVariety(models.Model):
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='chais/')
-    date_add = models.DateTimeField(default=timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='ML')
     description = models.TextField(default='')
 
     def __str__(self):
         return self.name
+
+# One to Many relationship
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+    
+# Many to Many relationship
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+# One to One relationship
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
